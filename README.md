3505706cb7dc32a3251a186bc463a56dd194a345
shikeik <2667097598@qq.com>
2024-11-08 14:55:13 +0800

    0.2.0

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# ConsoleTextMap
+# ConsoleTextMap V0.2.0
 简易控制台文字地图
 
 # 当前内容：
- 简易控制台文字地图
- x y回车改变主角位置
+简易控制台文字地图
+ 操作方式：
+ 	方向数字+距离移动玩家：2为上1, 42为左2
+代码结构: 
+	launchGame()启动
+		initMap()初始化地图
+		clearLine(2000)控制台光标置底
+		renderMap()渲染初始地图
+		while()游戏循环
+			inputHandle()阻塞式输入处理
+			renderMap()刷新渲染地图
+异常处理：
+	顶级try-catch保证捕获所有异常
+	内部所有异常软处理以保证游戏正常运行
+	异常输出在LogErr
 
--- src/MainA.java
+++ src/MainA.java
@@ -4,13 +4,34 @@ import java.util.*;
 
 /*
  简易控制台文字地图
- x y回车改变主角位置
+ 操作方式：
+ 	方向数字+距离移动玩家：2为上1, 42为左2
+代码结构: 
+	launchGame()启动
+		initMap()初始化地图
+		clearLine(2000)控制台光标置底
+		renderMap()渲染初始地图
+		while()游戏循环
+			inputHandle()阻塞式输入处理
+			renderMap()刷新渲染地图
+异常处理：
+	顶级try-catch保证捕获所有异常
+	内部所有异常软处理以保证游戏正常运行
+	异常输出在LogErr
  */
 public class MainA 
 {
-	String shapes = "～＆";
+	String shapeSet = "～＆";
+	public void setShapeSet(String shapeSet){ this.shapeSet = shapeSet; }
 	int clearId = 0;
 	int playerId = 1;
+	//方向代码->坐标偏移 上左下右2486
+	Map<Integer, int[]> directionMap = new HashMap<Integer, int[]>(){{
+			put(2, new int[]{0, 1});
+			put(4, new int[]{-1, 0});
+			put(8, new int[]{0, -1});
+			put(6, new int[]{1, 0});
+		}};
 
 	int size = 12;
 	int totalSize = size * size;
@@ -19,14 +40,14 @@ public class MainA
 
 	public MainA(){
 		try{
-			gameLoop();
+			launchGame();
 		}catch (Exception e){
 			LogErr("游戏主循环发生异常: ", e);
 		}
 	}
 
-	public void gameLoop(){
-		initGame();
+	public void launchGame(){
+		initMap();
 
 		//将光标置底，控制台上限2000行
 		clearLine(2000);
@@ -41,7 +62,8 @@ public class MainA
 	}
 
 
-	public void initGame(){
+	//初始化地图
+	public void initMap(){
 		//实例化地图
 		textMap = new TextMap(clearId, size);
 
@@ -52,8 +74,8 @@ public class MainA
 	}
 
 
+	//渲染地图
 	public void renderMap(){
-		//渲染地图
 		String mapRenderer="", lineRenderer="";
 		int[] mapData = textMap.getMap();
 		for (int i=0;i < totalSize;i++){
@@ -74,43 +96,62 @@ public class MainA
 		Log(mapRenderer);
 	}
 
-
+	//处理输入
 	public void inputHandle(){
-		//获取指令列表
+		//分割指令
 		String text = getInput();
 		String[] cmds = text.split(" ");
 
 		//指令处理
-		if (cmds.length > 1){
-			int x = Integer.parseInt(cmds[0]);
-			int y = Integer.parseInt(cmds[1]);
-			//设置主角
-			setPos(x, y, playerId);
-			Log(String.format("主角位置更新：x:%d, y:%d", x, y));
+		//移动主角: 2为上移1格 42为左移2格
+		try{
+			int direction = Integer.parseInt("" + text.charAt(0));
+			int distance = text.length()==1?1: Integer.parseInt(text.substring(1, text.length()));
+			movePlayer(direction, distance);
+		}catch (Exception e){
+			LogErr("移动指令异常: ", e);
 		}
+//		直接设置主角坐标
+//		if (cmds.length > 1){
+//			int x = Integer.parseInt(cmds[0]);
+//			int y = Integer.parseInt(cmds[1]);
+//			//xy转索引
+//			x -= 1;y -= 1;
+//			setPlayerPos(x, y);
+//			Log(String.format("主角位置更新：x:%d, y:%d", x, y));
+//		}
 	}
-
 
 	//从id获取渲染形状
 	private String getShapeById(int id)
 	{
 		String ret="？";
 		try{
-			ret = "" + shapes.charAt(id);
+			ret = "" + shapeSet.charAt(id);
 		}catch (Exception e){
 			LogErr(String.format("返回形状失败，自动转为%s, 不存在的形状id:%d", ret, id), e);
 		}
 		return ret;
 	}
 
-	public void setPos(int x, int y, int id){
-		//xy转索引
-		x -= 1;y -= 1;
+	//设置玩家位置
+	public void setPlayerPos(int x, int y){
 		if (x < 0 || x >= size || y < 0 || y >= size)
 			LogErr(String.format("设置位置失败, xy坐标越界:%d,%d", x, y), new Exception());
 		else{
 			player[0] = x;player[1] = y;
 		}
+	}
+	
+	//移动玩家，指定方向数字与距离
+	public void movePlayer(int directionNumber, int distance) throws NoSuchElementException{
+		if (!directionMap.containsKey(directionNumber)){
+			throw new NoSuchElementException("方向表没有方向数字:"+directionNumber);
+		}
+		int[] coordOffset = directionMap.get(directionNumber).clone();
+		coordOffset[0] *= distance;
+		coordOffset[1] *= distance;
+		setPlayerPos(player[0]+coordOffset[0], player[1]+coordOffset[1]);
 	}
 
 
